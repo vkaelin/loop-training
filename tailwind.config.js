a8b2472086cedc8447dc7b992a5053ffca21722a
Valentin Kaelin <kalanehd@gmail.com>
2020-05-06 13:35:19 +0200

    fix: add purgecss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,5 +18,15 @@ module.exports = {
   variants: {},
   plugins: [
     require('@tailwindcss/ui')
-  ]
+  ],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
 }
