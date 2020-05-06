c0a5d3ffb7f6e8f426760ee5f1a9d3cadf360fa8
Valentin Kaelin <kalanehd@gmail.com>
2020-05-06 13:29:58 +0200

    feat: add nuxt boilerplate with tailwindcss and fonts

---
+++ nuxt.config.js
@@ -0,0 +1,59 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap' },
+      { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/pwa'
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container mx-auto mt-20">
+    <div>
+      <h1 class="text-4xl text-purple-500 font-title">
+        Loop
+      </h1>
+      <h2 class="">
+        Application faciliant l'entraînement fractionné
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
