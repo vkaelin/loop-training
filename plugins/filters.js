24dde3f6efa0eb43b3ae2a30813cc38bb8c2f2eb
Valentin Kaelin <kalanehd@gmail.com>
2020-05-08 16:24:04 +0200

    fix: hopefully last fix for timer display

--- plugins/filters.js
+++ plugins/filters.js
@@ -3,6 +3,10 @@ import Vue from 'vue'
 Vue.filter('secToTime', (sec, ceil = false) => {
   let min = Math.floor(sec / 60)
   let newSec = ceil ? Math.ceil(sec - min * 60) : Math.floor(sec - min * 60)
+  if (newSec === 60) {
+    newSec = 0
+    min++
+  }
   newSec = newSec < 10 ? '0' + newSec : newSec
   min = min < 10 ? '0' + min : min
 
